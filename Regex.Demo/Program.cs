8d70d0d3e3f41b8c18a4a890b93a1b065d56de3b
PanPan <hupan19931014@vip.qq.com>
2018-07-21 09:56:36 +0800

    正则表达式"()"括号应用

--- Regex.Demo/Program.cs
+++ Regex.Demo/Program.cs
@@ -2,13 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Regex.Demo
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+
+            const string strSource = @"[AA1]+[B1]+[A330]+[BZ1]";
+            var regex = new System.Text.RegularExpressions.Regex(@"(\[)([A-Z]+)(\d+)(\])(.?)");
+            var matchs = regex.Matches(strSource);
+            var i = 0;
+            var result = string.Empty;
+            foreach (Match match in matchs)
+            {
+                i++;
+                var s0 = match.Groups[0].Value;
+                var s1 = match.Groups[1].Value;
+                var s2 = match.Groups[2].Value;
+                var s3 = match.Groups[4].Value;
+                var s4 = match.Groups[5].Value;
+
+                result += $"{s1}{s2}{i}{s3}{s4}";
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
